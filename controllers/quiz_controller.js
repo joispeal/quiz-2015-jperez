eb0967e189da0a877e3078510582e07d6c1201b8
Jorge Isaac Perez Alvarez <joispeal@gmail.com>
2015-08-04 15:55:26 -0500

    Editar preguntas
    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,7 +9,7 @@ function CleanSearch(search) {
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
+  models.Quiz.findById(quizId).then(
     function(quiz) {
       if (quiz) {
         req.quiz = quiz;
@@ -76,5 +76,31 @@ exports.create = function(req, res) {
 				});
 			} // res.redirect: Redireccion HTTP (URL relativo) lista de preguntas
     }
+	);
+};
+
+//GET /quizes/ :id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // Autoload de instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/ :id
+exports.update = function(req, res) {
+	req.quiz.pregunta  = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err) {
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: []});
+			} else {
+				req.quiz // save: guarda campos pregunta y respuesta en DB
+				.save( {fields: ["pregunta", "respuesta"]})
+				.then( function() { res.redirect('/quizes');});
+			}  // Redireccion HTTP a lista de preguntas (URL relativo)
+		}
 	);
 };
