08fbe41c68c138b572ec5cd19ca4c8d52735997a
Jorge Isaac Perez Alvarez <joispeal@gmail.com>
2015-08-04 16:16:48 -0500

    Borrar Preguntas
    Borrar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -104,3 +104,10 @@ exports.update = function(req, res) {
 		}
 	);
 };
+
+// DELETE /quizes/ :Id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then( function() {
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
